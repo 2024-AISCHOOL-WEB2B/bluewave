6eba515b0eb638b755bc4c4457b47e3c06cfe1f0
Jipumpkin <qsoqso24@gmail.com>
2024-07-27 19:01:52 +0900

    정책 카테고리코드 변환 기능

---
+++ BlueWave/src/main/java/com/util/PolicyUtils.java
@@ -0,0 +1,28 @@
+package com.util;
+
+public class PolicyUtils {
+
+    public static String getCategory(String policyFieldCode) {
+        String category = "-";
+        
+        switch (policyFieldCode) {
+            case "23010":
+                category = "일자리 분야";
+                break;
+            case "23020":
+                category = "주거 분야";
+                break;
+            case "23030":
+                category = "교육 분야";
+                break;
+            case "23040":
+                category = "복지.문화 분야";
+                break;
+            case "23050":
+                category = "참여.권리 분야";
+                break;
+        }
+        
+        return category;
+    }
+}
