784c07cea7cd00dcf4fdb2941e7c788d440ef9f6
YooooSW <pk8652@naver.com>
2024-07-29 15:15:33 +0900

    지역 코드 추가

--- BlueWave/src/main/java/com/util/CodeUtil.java
+++ BlueWave/src/main/java/com/util/CodeUtil.java
@@ -9,14 +9,23 @@ public class CodeUtil {
 
     static {
         // 거주지역 코드 매핑
-        regionCodeMap.put("강원", "3001001");
-        regionCodeMap.put("광주", "3001002");
-        regionCodeMap.put("경북", "3001003");
-        regionCodeMap.put("인천", "3001004");
-        regionCodeMap.put("전남", "3001005");
-        regionCodeMap.put("대전", "3001006");
-        regionCodeMap.put("경남", "3001007");
-        regionCodeMap.put("서울", "3001008");
+    	regionCodeMap.put("서울", "3002001");
+        regionCodeMap.put("부산", "3002002");
+        regionCodeMap.put("대구", "3002003");
+        regionCodeMap.put("인천", "3002004");
+        regionCodeMap.put("광주", "3002005");
+        regionCodeMap.put("대전", "3002006");
+        regionCodeMap.put("울산", "3002007");
+        regionCodeMap.put("경기", "3002008");
+        regionCodeMap.put("강원", "3002009");
+        regionCodeMap.put("충북", "3002010");
+        regionCodeMap.put("충남", "3002011");
+        regionCodeMap.put("전북", "3002012");
+        regionCodeMap.put("전남", "3002013");
+        regionCodeMap.put("경북", "3002014");
+        regionCodeMap.put("경남", "3002015");
+        regionCodeMap.put("제주", "3002016");
+        regionCodeMap.put("세종", "3002017");
         // 필요에 따라 추가(중앙부처등의 코드)
 
         // 관심정책분야 코드 매핑
